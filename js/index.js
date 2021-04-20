2b188b975a5ffd6d713c5bb03c557665220539bc
denis salvino <denis.salvino23@gmail.com>
2021-04-20 17:49:25 -0500

    yabadabadoo

--- js/index.js
+++ js/index.js
@@ -2,15 +2,24 @@ $('select[name="form-change"]').on('change', function () {
     const switchForms = $(this).val();
     switch (switchForms) {
         case 'Add':
-            $('.updateCall').hide();
             $('.addCall').show();
+            $('.updateCall').hide();
             break;
         case 'Update':
+            $('.addCall').hide();
             $('.updateCall').show();
-            $('.AddCall').hide();
             break;
         default:
             $('.updateCall').hide();
             $('.AddCall').hide();
     }
 });
+
+// $('select[name="form-change"]').change(function() {
+//     if ($(this).val() === "Add") {
+//         $('#addCall').show();
+//         $('#updateCall').hide();
+//     } else {
+//         $
+//     }
+// })
