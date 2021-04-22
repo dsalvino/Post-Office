5b7cf5367047d52f9283c86ab468de0c3de5dff6
denis salvino <denis.salvino23@gmail.com>
2021-04-21 19:35:45 -0500

    updated signup to contain checkboxes for employee/customer; also updated index to contain a logout that redirects to the login page; also added a login page

--- js/index.js
+++ js/index.js
@@ -26,3 +26,34 @@ $("#customer-form").on("submit", function (e) {
     });
     e.preventDefault();
 });
+
+//show packages
+$(document).ready(function () {
+    var API_URL = "https://dw5ii1jbse.execute-api.us-east-2.amazonaws.com/test/list-packages";
+    $.ajax({
+        url: API_URL, success: function (res) {
+            for (var i = 0; i < res.length; i++) {
+                $("#package-table").append("<tr>");
+                for (var j = 0; j < res[i].length; j++)
+                    $("#package-table tr:last").append("<td>" + res[i][j] + "</td>");
+                $("#package-table").append("</tr>")
+            }
+        }
+    });
+});
+
+//po viewer
+$(document).ready(function () {
+    var API_URL = "https://dw5ii1jbse.execute-api.us-east-2.amazonaws.com/test/post-offices";
+    $.ajax({
+        url: API_URL, success: function (res) {
+            for (var i = 0; i < res.length; i++) {
+                $("#post-office-table").append("<tr>");
+                for (var j = 0; j < res[i].length; j++)
+                    $("#post-office-table tr:last").append("<td>" + res[i][j] + "</td>");
+                $("#post-office-table tr:last").append("<td><a href=\'./showpackages.html?id=" + res[i][0] + "\'>" + "Show Packages" + "</a></td>")
+                $("#post-office-table").append("</tr>")
+            }
+        }
+    });
+});
