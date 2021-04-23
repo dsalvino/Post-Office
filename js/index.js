e5534efbd0a402ed18e1e87a5ab4bfe66db6b290
Andrew <abarrett879@gmail.com>
2021-04-22 21:01:09 -0500

    CJ's 4/22 update

--- js/index.js
+++ js/index.js
@@ -1,15 +1,39 @@
 var API_URL = "https://i645of8oc3.execute-api.us-east-2.amazonaws.com/Beta/customers";
 
+
+//switch case for customer view
 $('select[name="form-change"]').on('change', function () {
     const switchForms = $(this).val();
     switch (switchForms) {
         case 'Add':
             $('.addCall').show();
-            $('.updateCall').hide();
+            $('.statusCall').hide();
+            $('.locationsCall').hide();
             break;
-        case 'Update':
+        case 'Track':
             $('.addCall').hide();
-            $('.updateCall').show();
+            $('.statusCall').show();
+            $('.locationsCall').hide();
+            break;
+        case 'Office':
+            $('.addCall').hide();
+            $('.statusCall').hide();
+            $('.locationsCall').show();
+            break;
+    }
+});
+
+//switch case for employee view
+$('select[name="form-change"]').on('change', function () {
+    const switchForms = $(this).val();
+    switch (switchForms) {
+        case 'viewPkgLocations':
+            $('.viewPkgLocationsCall').show();
+            $('.killEmployeeCall').hide();
+            break;
+        case 'delete':
+            $('.viewPkgLocationsCall').hide();
+            $('.killEmployeeCall').show();
             break;
     }
 });
@@ -25,4 +49,35 @@ $("#customer-form").on("submit", function (e) {
         }
     });
     e.preventDefault();
+});
+
+//show packages
+$(document).ready(function () {
+    var API_URL = "https://dw5ii1jbse.execute-api.us-east-2.amazonaws.com/test/list-packages";
+    $.ajax({
+        url: API_URL, success: function (res) {
+            for (var i = 0; i < res.length; i++) {
+                $("#package-table").append("<tr>");
+                for (var j = 0; j < res[i].length; j++)
+                    $("#package-table tr:last").append("<td>" + res[i][j] + "</td>");
+                $("#package-table").append("</tr>")
+            }
+        }
+    });
+});
+
+//po viewer
+$(document).ready(function () {
+    var API_URL = "https://dw5ii1jbse.execute-api.us-east-2.amazonaws.com/test/post-offices";
+    $.ajax({
+        url: API_URL, success: function (res) {
+            for (var i = 0; i < res.length; i++) {
+                $("#post-office-table").append("<tr>");
+                for (var j = 0; j < res[i].length; j++)
+                    $("#post-office-table tr:last").append("<td>" + res[i][j] + "</td>");
+                $("#post-office-table tr:last").append("<td><a href=\'./showpackages.html?id=" + res[i][0] + "\'>" + "Show Packages" + "</a></td>")
+                $("#post-office-table").append("</tr>")
+            }
+        }
+    });
 });
